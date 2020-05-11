1643fc8a73df7c47187e74f828c508a6b3228384
Micah <Rus.Micah@gmail.com>
2020-05-11 15:00:34 +1000

    Added new/create to routes for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index]
-  resources :games, only: [:show]
+  resources :games, only: [:show, :new, :create]
   root to: "users#index"
 end
