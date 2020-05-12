3236817566d52dee3c369433cf23c8c4ad0d7da2
Micah <Rus.Micah@gmail.com>
2020-05-12 11:02:50 +1000

    Rubocopped

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     user ||= User.new
     can [:index, :show, :new, :create], Game
     can [:edit, :update, :destroy], Game, user_id: user.id
-
   end
 end
