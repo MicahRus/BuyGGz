6e742e0fc30bb8333008a3b2bec3272aaf255934
Micah <Rus.Micah@gmail.com>
2020-05-12 14:54:46 +1000

    Added images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-
+counter = 0
+counter_2 = 3
 
 puts '*' * 24
 
@@ -16,21 +17,21 @@ User.create({
   username: "test_user_1",
   email: "Test_user_1@gmail.com",
   password: "password",
-  profile_pic: Faker::LoremPixel.image
+  profile_pic: nil
 })
 
 User.create({
   username: "test_user_2",
   email: "Test_user_2@gmail.com",
   password: "password",
-  profile_pic: Faker::LoremPixel.image
+  profile_pic: nil
 })
 
 User.create({
   username: "admin",
   email: "admin@gmail.com",
   password: "password",
-  profile_pic: Faker::LoremPixel.image,
+  profile_pic: nil,
   is_admin: true
 })
 
@@ -42,28 +43,34 @@ puts ""
 puts 'Seeding Games'
 
 3.times do
+counter += 1
+counter_2 += 1
 random_price = rand(1000..15000).round(-1)
 random_price_2 = rand(1000..15000).round(-1)
-  Game.create({
+
+
+  game = Game.create({
     user_id: 1,
     title: Faker::Game.title,
     cost: random_price,
     platform: Faker::Game.platform,
-    image: nil ,
+    image: nil,
     posted_by: 'test_user_1'
 
   })
 
-    Game.create({
+  game.image.attach(io: File.open("app/assets/images/pic#{counter}.jpeg"), filename: "pic#{counter}.jpeg") if game['image'].nil?
+
+  game = Game.create({
     user_id: 2,
     title: Faker::Game.title,
     cost: random_price_2,
     platform: Faker::Game.platform,
-    image: nil ,
+    image: nil,
     posted_by: 'test_user_2'
 
   })
-
+  game.image.attach(io: File.open("app/assets/images/pic#{counter_2}.jpeg"), filename: "pic#{counter_2}.jpeg") if game['image'].nil?
 
 
 
