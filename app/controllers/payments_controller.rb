a4f2769fe4ea175dcbd26951dc4f6cdafbb660ef
Micah <Rus.Micah@gmail.com>
2020-05-14 15:18:42 +1000

    Rubocopped

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,4 @@
 class PaymentsController < ApplicationController
-
   skip_before_action :verify_authenticity_token, only: [:webhook]
   def success
     @cart = current_user.cart
@@ -17,35 +16,33 @@ class PaymentsController < ApplicationController
 
     head 200
   end
-  
 
   def get_stripe_id
-  counter = 0
-  line_items = []
+    counter = 0
+    line_items = []
     @cart = Cart.find(current_user.cart.id)
-          @cart.games.length.times do 
-            hash = { name: @cart.games[counter].title,
-            amount: @cart.games[counter].cost,
-            currency: 'aud',
-            quantity: 1}
-            line_items << hash
-            counter += 1
-          end
-        session_id = Stripe::Checkout::Session.create(
-          payment_method_types: ['card'],
-          customer_email: current_user.email,
-          line_items: line_items,
+    @cart.games.length.times do
+      hash = { name: @cart.games[counter].title,
+               amount: @cart.games[counter].cost,
+               currency: 'aud',
+               quantity: 1 }
+      line_items << hash
+      counter += 1
+    end
+    session_id = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      customer_email: current_user.email,
+      line_items: line_items,
 
-        
-        payment_intent_data: {
-          metadata: {
-            user_id: current_user.id,
-            cart_id: @cart.user_id
-          }
-        },
-        success_url: "#{root_url}payments/success?userId=#{current_user.id}&gameId=#{@cart.id}",
-        cancel_url: "#{root_url}games"
-      ).id
-      render :json => { id: session_id, stripe_public_key: Rails.application.credentials.dig(:stripe, :public_key) }
+      payment_intent_data: {
+        metadata: {
+          user_id: current_user.id,
+          cart_id: @cart.user_id
+        }
+      },
+      success_url: "#{root_url}payments/success?userId=#{current_user.id}&gameId=#{@cart.id}",
+      cancel_url: "#{root_url}games"
+    ).id
+    render :json => { id: session_id, stripe_public_key: Rails.application.credentials.dig(:stripe, :public_key) }
   end
 end
