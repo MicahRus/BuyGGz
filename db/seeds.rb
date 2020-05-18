2723ed091bf665efac286c85521d1e2992df8a72
Micah <Rus.Micah@gmail.com>
2020-05-18 15:11:37 +1000

    Added pagenisation, 'ugly though'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 
 counter = 0
 counter_2 = 3
+counter_3 =1
+random_platform = rand(1..30)
 
 @platforms = [
   "macOS", "Game Boy Advance", "Xbox One", "Xbox", "Windows", "Nintendo DS", "GameCube", "Nintendo 64", "PlayStation Portable", "Game Gear", "PlayStation 2", "Nintendo DSi", "Sega Master System", "Super Nintendo Entertainment System", "Virtual Boy", "Wii", "Xbox 360", "Game Boy", "PlayStation 3", "PlayStation 4", "PlayStation Vita", "Game Boy Color", "Linux", "Sega Dreamcast", "Sega Genesis", "Nintendo Entertainment System", "Nintendo Switch", "Wii U", "Nintendo 3DS", "PlayStation", "Sega Saturn"
@@ -59,16 +61,7 @@ random_price = rand(1000..15000).round(-1)
 random_price_2 = rand(1000..15000).round(-1)
 
 
-  2.times do 
-    Game.create({
-      user_id: counter + 1,
-      title: Faker::Game.unique.title,
-      cost: random_price,
-      platform: @platforms[rand(0..31)],
-      image: nil,
-      posted_by: User.find(counter + 1).username
-    })
-  end
+
   game = Game.create({
     user_id: 1,
     title: Faker::Game.title,
@@ -91,10 +84,25 @@ random_price_2 = rand(1000..15000).round(-1)
 
   })
   game.image.attach(io: File.open("app/assets/images/pic#{counter_2}.jpeg"), filename: "pic#{counter_2}.jpeg") if game['image'].nil?
-
-
-
 end
+
+  9.times do 
+    20.times do 
+    random_price = rand(1000..15000).round(-1)
+      Game.create({
+        user_id: counter_3,
+        title: Faker::Game.unique.title,
+        cost: random_price,
+        platform: @platforms[rand(1..30)],
+        image: nil,
+        posted_by: User.find(counter_3).username
+      })
+    end
+      counter_3 += 1
+  end
+
+
+
 
 puts 'Games successfully seeded ✅'
 puts '*' * 24
